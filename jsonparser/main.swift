912b3d16be96610e2705d268876551c1c2f4c004
Daniel Jilg <winsmith@winsmith.de>
2020-05-06 20:51:07 +0200

    Initial Commit

---
+++ jsonparser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  jsonparser
+//
+//  Created by Daniel Jilg on 06.05.20.
+//  Copyright © 2020 Daniel Jilg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
